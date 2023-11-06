76e35c2e24d6a53ceb9166db89f8c8196fd4ad60
Kajal <Kajal.mittal08@gmail.com>
2023-11-06 23:32:50 +0530

    Added buttons

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const articlesRouter = require("./routes/articles");
 const Article = require("./models/articles");
+const methodOverride = require('method-override')
 const app = express()
 mongoose.connect("mongodb://localhost:27017/blog")
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use("/articles", articlesRouter)
-
+app.use(methodOverride('_method'))
 app.get("/", async(req, res) => {
     const articles = await Article.find().sort({ createdAt: "desc" });
     res.render("articles/index", { articles: articles });
 })
+app.use("/articles", articlesRouter)
 
 app.listen(5000);
